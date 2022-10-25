e56f7a65daee47d27084a771108960a43795c710
Taylor Vesely <tvesely@yugabyte.com>
2022-10-25 11:15:40 -0700

    yugaware-client: backup create command

--- yugaware-client/pkg/client/swagger/client/backups/create_multi_table_backup_responses.go
+++ yugaware-client/pkg/client/swagger/client/backups/create_multi_table_backup_responses.go
@@ -29,6 +29,12 @@ func (o *CreateMultiTableBackupReader) ReadResponse(response runtime.ClientRespo
 			return nil, err
 		}
 		return result, nil
+	case 400:
+		result := NewCreateMultiTableBackupBadRequest()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return nil, result
 	default:
 		return nil, runtime.NewAPIError("response status code does not match any response statuses defined for this endpoint in the swagger spec", response, response.Code())
 	}
@@ -41,22 +47,54 @@ func NewCreateMultiTableBackupOK() *CreateMultiTableBackupOK {
 
 /* CreateMultiTableBackupOK describes a response with status code 200, with default header values.
 
-If requested schedule backup.
+If requested schedule backup. Changes from upstream: This API call actually returns a task, not a Schedule
 */
 type CreateMultiTableBackupOK struct {
-	Payload *models.Schedule
+	Payload *models.YBPTask
 }
 
 func (o *CreateMultiTableBackupOK) Error() string {
 	return fmt.Sprintf("[PUT /api/v1/customers/{cUUID}/universes/{uniUUID}/multi_table_backup][%d] createMultiTableBackupOK  %+v", 200, o.Payload)
 }
-func (o *CreateMultiTableBackupOK) GetPayload() *models.Schedule {
+func (o *CreateMultiTableBackupOK) GetPayload() *models.YBPTask {
 	return o.Payload
 }
 
 func (o *CreateMultiTableBackupOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
 
-	o.Payload = new(models.Schedule)
+	o.Payload = new(models.YBPTask)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+// NewCreateMultiTableBackupBadRequest creates a CreateMultiTableBackupBadRequest with default headers values
+func NewCreateMultiTableBackupBadRequest() *CreateMultiTableBackupBadRequest {
+	return &CreateMultiTableBackupBadRequest{}
+}
+
+/* CreateMultiTableBackupBadRequest describes a response with status code 400, with default header values.
+
+When request fails validations. Changes from upstream: Return code 400 is not in swagger spec
+*/
+type CreateMultiTableBackupBadRequest struct {
+	Payload *models.YBPError
+}
+
+func (o *CreateMultiTableBackupBadRequest) Error() string {
+	return fmt.Sprintf("[PUT /api/v1/customers/{cUUID}/universes/{uniUUID}/multi_table_backup][%d] createMultiTableBackupBadRequest  %+v", 400, o.Payload)
+}
+func (o *CreateMultiTableBackupBadRequest) GetPayload() *models.YBPError {
+	return o.Payload
+}
+
+func (o *CreateMultiTableBackupBadRequest) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models.YBPError)
 
 	// response payload
 	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
