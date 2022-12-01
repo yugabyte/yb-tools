d9fcda2039ce3aae2e92f9731b084db9f15965fd
Jake Angerman <jangerman@yugabyte.com>
2022-12-01 15:21:20 -0500

    heatseeker: handle units in sst and wal sizes

--- tablet-report-parser/heatseeker.pl
+++ tablet-report-parser/heatseeker.pl
@@ -2,6 +2,10 @@
 # heatseeker: find hot shards by aggregating disk usage by tablet by node
 # parses output of 'yugatool cluster_info --tablet-report'
 
+@units = ('b', 'kb', 'mb', 'gb', 'tb');
+@exponents = 0 .. $#units;
+@exponent{@units} = @exponents;
+
 while (<>) {
 	# skip the beginning sections and empty lines
 	next if (m/^$/);
@@ -12,19 +16,19 @@ while (<>) {
 	}
 	next unless $seen_it;
 
-	($tablet, $table, $namespace, $state, $status, $start_key, $end_key, $sst_size, $wal_size, $cterm, $cidx, $leader, $lease_status) = split(' ');
+	($tablet, $table, $namespace, $state, $status, $start_key, $end_key, $sst_size, $sst_units, $wal_size, $wal_units, $cterm, $cidx, $leader, $lease_status) = split(' ');
 	next if ($tablet eq "TABLET"); # skip header line
 
-	$summation{$table}{"$tablet,$host"} += $sst_size + $wal_size;
+	$summation{$table}{"$tablet,$host"} += ($sst_size * 1024**$exponent{lc $sst_units}) + ($wal_size * 1024**$exponent{lc $wal_units});
 }
 
 foreach $table (keys %summation) {
 	foreach $tuple (keys %{$summation{$table}}) {
 		# build an array of sums for each table/tablet corresponding to each host
-		$kb = $summation{$table}{$tuple};
+		$size = $summation{$table}{$tuple};
 		($tablet, $host) = split(/,/, $tuple);
-		push(@{$report{$table}{$tablet}}, $kb);
-		$digits = $kb > 0 ? int(log($kb)/log(10))+1 : 0;
+		push(@{$report{$table}{$tablet}}, $size);
+		$digits = $size > 0 ? int(log($size)/log(10))+1 : 0;
 		$digits{$table} = $digits if ($digits > $digits{$table}); # max()
 	}
 }
@@ -33,10 +37,10 @@ foreach $table (keys %summation) {
 	print "$table (" . scalar(keys %{$report{$table}}) . " shards)\n"; 
 	foreach $tablet (keys %{$report{$table}}) {
 		print "$tablet ";
-		@kb = @{$report{$table}{$tablet}};
+		@size = @{$report{$table}{$tablet}};
 		$format = sprintf "%%%sd, ", $digits{$table};
-		printf "$format", $_ for @kb[0..$#kb-1];
+		printf "$format", $_ for @size[0..$#size-1];
 		$format =~ s/,.*//;
-		printf "$format\n", @kb[$#kb];
+		printf "$format\n", @size[$#size];
 	}
 }
