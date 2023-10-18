c320d54455401c61b14239f1e87b8bb14b8e3c1b
Ashok Gangwar <ag26jan@gmail.com>
2023-10-18 13:27:30 +0530

    Updating dump_analyser.sh to handle some edge case where the binary info is not correctly handled
    In some of the edge case the binary have truncated path like below:
    
    ```
    error: file specified in --file (-f) option doesn't exist: '/home/yugabyte/yb-software/yugabyte-2.19.0.0-b190-centos-x
    86_64/postgres/bin/po'
    ```
    
    Which results in failure as there is no `/bin/po` util. This should be `/bin/postgres` . 
    
    So the script has been modified (Credit: Eugene) to filter the correct name with command.
    
    ```
    diff /home/support/bin/dump_analyser.sh.20230718 /home/support/bin/dump_analyser.sh
    51,55c51
    < #strings "$file_name" | grep -E "/yugabyte/yb-software/yugabyte-" | head -n 1 > /tmp/yb_db_version & PID=$!
    <
    < strings "$file_name" | grep -P "^/.*/yugabyte/yb-software/.*/bin/" | sort | uniq -c | sort -nr | head -1 | awk {'print $2'} > /tmp/yb_db_version & PID=$!
    <
    <
    ---
    > strings "$file_name" | grep -E "/yugabyte/yb-software/yugabyte-" | head -n 1 > /tmp/yb_db_version & PID=$!
    66,68d61
    <
    <
    ```

--- dump_analyser.sh
+++ dump_analyser.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
+#Function to show blinkdots for various steps in progress when needed in the script.
+
+function blinkdots() {
+  local pid=$1
+  local delay=0.5
+  while kill -0 $pid 2>/dev/null; do
+    printf "."
+    sleep $delay
+    printf "."
+    sleep $delay
+    printf "."
+    sleep $delay
+    printf "\b\b\b   \b\b\b"
+    sleep $delay
+  done
+  printf "\n"
+}
+
+
 # Function to display error message and exit
+
 function error_exit() {
   echo "$1"
   exit 1
@@ -10,11 +30,13 @@ echo "Enter the Core dump file name: "
 read -r file_name
 
 # Check if the Core file exists
+
 if [ ! -f "$file_name" ]; then
   error_exit "Error: The file '$file_name' does not exist."
 fi
 
 # Check if the Core file is a valid core dump
+
 file_type=$(file "$file_name")
 if echo "$file_type" | grep -q "core file"; then
   echo "Great! The core dump file provided is a valid core dump, proceeding with analysis of core dump."
@@ -22,16 +44,44 @@ else
   error_exit "Error: The Core dump file is NOT an ELF core dump, please provide a valid core dump file. Make sure the file IS NOT compressed, if so please extract it and then try again!"
 fi
 
+
 # Extract the yugabyte binary version information from the core dump file
-yb_db_version=$(strings "$file_name" | grep -E "/yugabyte/yb-software/yugabyte-" | head -n 1) || yb_db_version=""
+
+echo "Extracting yugabyte binary version information, please wait..."
+#strings "$file_name" | grep -E "/yugabyte/yb-software/yugabyte-" | head -n 1 > /tmp/yb_db_version & PID=$!
+
+strings "$file_name" | grep -P "^/.*/yugabyte/yb-software/.*/bin/" | sort | uniq -c | sort -nr | head -1 | awk {'print $2'} > /tmp/yb_db_version & PID=$!
+
+
+blinkdots $PID
+yb_db_version=$(cat /tmp/yb_db_version)
+rm /tmp/yb_db_version
+echo "Yugabyte binary version extraction completed."
+if [ -n "$yb_db_version" ]; then
+  yb_db_tar_file=$(echo "$yb_db_version" | awk -F "/" '{for (i=1; i<=NF; i++) if ($i == "yugabyte" && $(i+1) == "yb-software") print $(i+2)}' | sed 's/centos/linux/' | sed 's/$/.tar.gz/')
+  version=$(echo "$yb_db_tar_file" | awk -F "-" '{print $2}')
+else
+  error_exit "Error: Failed to extract Yugabyte binary version information."
+fi
+
+
+
+
+# Separator
+echo "--------------------------------------------------------"
+
+#Manupulating the above output to desired downloadable tar file name.
 
 yb_db_tar_file=$(echo "$yb_db_version" | awk -F "/" '{for (i=1; i<=NF; i++) if ($i == "yugabyte" && $(i+1) == "yb-software") print $(i+2)}' | sed 's/centos/linux/' | sed 's/$/.tar.gz/')
 
 version=$(echo "$yb_db_tar_file" | awk -F "-" '{print $2}')
 
+
 # Generate the URL for the yb-db binary
+
 yb_db_tar_url="https://downloads.yugabyte.com/releases/$version/$yb_db_tar_file"
 echo "The final URL is: $yb_db_tar_url"
+
 
 # Download the yb db binary tar file
 
@@ -51,27 +101,16 @@ else
   fi
 fi
 
+# Separator
+echo "--------------------------------------------------------"
+
 #Renamed executable dir, means we are going to extract the tar file binary in the same version format as core file.
 
 executable_dir=$(echo "$yb_db_version" | awk -F "/" '{for (i=1; i<=NF; i++) if ($i == "yugabyte" && $(i+1) == "yb-software") print $(i+2)}')
 
 
-# Function to display a rotating symbol
-function spinner {
-    local pid=$!
-    local delay=0.75
-    local spinstr='|/-\'
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-        local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf "\b\b\b\b\b\b"
-    done
-    printf "    \b\b\b\b"
-}
+# Extract the yb binary tar file in the "/home/yugabyte/yb-software" dir in case file server
 
-# Extract the binary
 tar_file="$yb_db_install_dir/$yb_db_tar_file"
 extracted_directory="$yb_db_install_dir/$executable_dir"
 
@@ -80,30 +119,45 @@ if [ -d "$extracted_directory" ]; then
 else
   echo "Extracting $tar_file in $yb_db_install_dir"
   mkdir -p $yb_db_install_dir/$executable_dir && tar -xzf "$tar_file" --strip-components=1 -C $_ &>/dev/null &
-  spinner
+  blinkdots $!
+  if [ $? -eq 0 ]; then
+    echo "Extracting $tar_file completed."
+  else
+    error_exit "Error: Failed to extract $tar_file."
+  fi
 fi
 
-# Execute post install script
+# Separator
+echo "--------------------------------------------------------"
+
+# Execute post install script. This will setup the yb-db executable files to work with core and other cluster related stuff.
+
 post_install="$yb_db_install_dir/$executable_dir/bin/post_install.sh"
 
 if [ -f "$post_install" ]; then
   echo "Executing post_install script to setup the binary as per core dump. Please bear with me!"
   $post_install &>/dev/null &
-  spinner
+  blinkdots $!
+  if [ $? -eq 0 ]; then
+    echo "Post-installation setup completed."
+  else
+    error_exit "Error: Failed to execute post_install script."
+  fi
 else
   error_exit "Error: $post_install not found."
 fi
 
 
-
 # Replace the path in the yb_db_version with the new target directory
+
 new_yb_db_version=$(echo "$yb_db_version" | awk -F "/yugabyte/yb-software" '{print "/yugabyte/yb-software"$2}' | sed "s|/yugabyte/yb-software/yugabyte-[^/]*|$yb_db_install_dir/$executable_dir|")
+
 # Use the lldb command with the new input string
 # Ask user to enetr available lldb command option for ease.
-
 #The below section is to ask few more user inputs and redirection etc.
 
-
+# Separator
+echo "--------------------------------------------------------"
 
 echo "Select an option for lldb command and press ENTER:"
 echo "1. bt"
@@ -122,6 +176,10 @@ while [[ ! "$option" =~ ^(1|2|3|4)$ ]]; do
   read -r option
 done
 
+# Separator
+echo "--------------------------------------------------------"
+
+
 if [ "$option" == "1" ]; then
   lldb_command="bt"
 elif [ "$option" == "2" ]; then
@@ -139,6 +197,9 @@ if [ "$option" != "4" ]; then
     echo "Do you want to redirect the output to a file? (y/n)"
     read -r redirect_output
   done
+
+# Separator
+echo "--------------------------------------------------------"
 
 
 if [ "$redirect_output" == "y" ]; then
