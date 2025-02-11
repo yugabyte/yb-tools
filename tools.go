b67ad3edee023852f48ab1e408d3f72cd24844f6
Ian Anderson <ianderson@yugabyte.com>
2025-02-11 13:30:09 -0500

    Re-add removed tools dependencies
    Re-add golangci-lint, ginko, and protoc-gen-go to tools.go for dependency tracking.
    
    Unfortunately go fmt does not properly handle tools.go (import "github.com/tool/..." is a program, not an importable package.
    
    It looks like this is only fixed in Go 1.24 which directly supports "tools" specifiers in go.mod.

--- tools.go
+++ tools.go
@@ -4,8 +4,11 @@
 package tools
 
 import (
-	_ "github.com/go-swagger/go-swagger"
-	_ "github.com/oapi-codegen/oapi-codegen/v2"
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
+	_ "github.com/onsi/ginkgo/ginkgo"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
 
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
