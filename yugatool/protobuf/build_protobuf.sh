96afd1b2c5f3d199b908ee2acc5d759dbdfbd070
Ian Anderson <ianderson@yugabyte.com>
2025-05-22 15:04:41 -0400

    yugatool: switch protobuf build script from xargs to for loop
    It's unclear what changed that led to the protobuf build script silently failing to generate new .pb.go files but switching to a for loop fixes this.

--- yugatool/protobuf/build_protobuf.sh
+++ yugatool/protobuf/build_protobuf.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -x
+#set -x
 
 ARGS=$(find . -iname "*.proto" -printf "%P\n" | perl -lane '/(.*)\/([^\/]*)\/([^\/]*).proto/; print "--go_opt=M$_=github.com/yugabyte/yb-tools/yugatool/api/$1/$2;$2 --ybrpc_opt=M$_=/$1/$2;$2"')
 
-
-find yb yugatool -iname *.proto | xargs protoc $ARGS --go_out=./../api/ --ybrpc_out=./../api/
+for protofile in $(find yb yugatool -iname *.proto); do
+  echo "Compiling $protofile"
+  protoc ${ARGS} --go_out=./../api/ --ybrpc_out=./../api/ --go_opt=paths=source_relative $protofile
+done
