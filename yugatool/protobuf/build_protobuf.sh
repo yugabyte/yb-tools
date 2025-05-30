b91d8b7c750ef0daaa0624330ddfa2daf984762c
Ian Anderson <ianderson@yugabyte.com>
2025-05-30 16:33:40 -0400

    yugatool: update protobuf build script to place all protos into their own modules
    Changes in the structure of the protobuf files to separate monolithic modules
    into smaller components resulted in dependency loops in the resulting
    generated golang code.
    
    This change alters the build script to place each of the smaller components
    into its own go module. This should consistently generate finer-grained
    golang modules and resolve these dependency loops.
    
    A future commit will correct the yugatool code that depends on the current
    module structure.

--- yugatool/protobuf/build_protobuf.sh
+++ yugatool/protobuf/build_protobuf.sh
@@ -2,9 +2,9 @@
 
 #set -x
 
-ARGS=$(find . -iname "*.proto" -printf "%P\n" | perl -lane '/(.*)\/([^\/]*)\/([^\/]*).proto/; print "--go_opt=M$_=github.com/yugabyte/yb-tools/yugatool/api/$1/$2;$2 --ybrpc_opt=M$_=/$1/$2;$2"')
+ARGS=$(find . -iname "*.proto" -printf "%P\n" | perl -lane '/(.*)\/(.*).proto/; print "--go_opt=M$_=github.com/yugabyte/yb-tools/yugatool/api/$1/$2 --ybrpc_opt=M$_=/$1/$2"')
 
 for protofile in $(find yb yugatool -iname *.proto); do
   echo "Compiling $protofile"
-  protoc ${ARGS} --go_out=./../api/ --ybrpc_out=./../api/ --go_opt=paths=source_relative $protofile
+  protoc ${ARGS} --go_out=./../api/ --ybrpc_out=./../api/ --go_opt=module=github.com/yugabyte/yb-tools/yugatool/api $protofile
 done
